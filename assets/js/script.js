fb6b081d2e6a2e802ca85a8a69d050b563d161a2
Hetal Patel <nphetal1905@gmail.com>
2019-07-13 11:26:19 -0400

    completed functionality of js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,70 @@
-var topics = ['dove', 'parrot', 'red robbin'];
+var topics = ['Flowers', 'parrot', 'Tree'];
 
-for (var i = 0; i < topics.length; i++) {
-    var dynamicBtn = $("<button>");
-    dynamicBtn.addClass('dynamicBtn');
-    dynamicBtn.attr('data-name', topics[i]);
-    dynamicBtn.text(topics[i]);
-    $("#btnDiv").append(dynamicBtn);
+function displayButton() {
+    for (var i = 0; i < topics.length; i++) {
+       
+        var dynamicBtn = $("<button>");
+        dynamicBtn.addClass('dynamicBtn');
+        dynamicBtn.attr('data-name', topics[i]);
+        dynamicBtn.text(topics[i]);
+       
+        $("#btnDiv").append(dynamicBtn);
+    }
 }
-
+displayButton();
 
 $("#btnDiv").on('click', '.dynamicBtn', function (e) {
     e.preventDefault();
     var queryParam = $(this).attr('data-name')
-    alert(queryParam);
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=" + queryParam;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RxnNW4PuvaqzjybzP2uuoQchC1dJ9wQW&q=" + queryParam + "&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        // data.image_url
-        var imageUrl = response.data.image_original_url;
-        var dynamicImage = $("<img>");
-        dynamicImage.addClass('imgClass');
-        dynamicImage.attr("src", imageUrl);
-        dynamicImage.attr("alt", queryParam +"image");
+
+        var dynamicImgDiv = $("<div>");
+        dynamicImgDiv.attr('id', 'dynamicImgDiv');
+        dynamicImgDiv.addClass('imgDivClass');
+        var imageUrl;
+        var dynamicImage;
+        var stillImageUrl;
+        for (var i = 0; i < response.data.length; i++) {
+
+            imageUrl = response.data[i].images.original.url;
+            stillImageUrl = response.data[i].images.original_still.url;
+            dynamicImage = $("<img>");
+            dynamicImage.addClass('imgClass');
+            dynamicImage.attr("src", imageUrl);
+            dynamicImage.attr("alt", queryParam + "image");
+            dynamicImage.attr('data-state', 'animate');
+            dynamicImage.attr('data-still', stillImageUrl);
+            dynamicImage.attr('data-animate', imageUrl);
 
 
-            $("#imgDiv").prepend(dynamicImage);     
+            dynamicImgDiv.append(dynamicImage);
+        }
+        $("#imgDiv").prepend(dynamicImgDiv);
+
+        $(".imgClass").on("click", function () {
+            var state = $(this).attr("data-state");
+
+            if (state === "animate") {
+                $(this).attr("src", $(this).attr('data-still'));
+                $(this).attr("data-state", "still");
+            } else {
+                $(this).attr("src", $(this).attr('data-animate'));
+                $(this).attr("data-state", "animate");
+            }
+        });
+
     });
-
 });
 
-
-//    $("#cat-button").on("click", function() {
-
-
-//     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats";
-
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// })
-
-//   .then(function(response) {
-
-
-//     var imageUrl = response.data.image_original_url;
-
-//     var catImage = $("<img>");
-
-
-//     catImage.attr("src", imageUrl);
-//     catImage.attr("alt", "cat image");
-
-
-//     $("#images").prepend(catImage);
-//   });
-
-//   $(".gif").on("click", function() {
-//     var state = $(this).attr("data-state");
-
-//     if (state === "still") {
-//       $(this).attr("src", $(this).attr("data-animate"));
-//       $(this).attr("data-state", "animate");
-//     } else {
-//       $(this).attr("src", $(this).attr("data-still"));
-//       $(this).attr("data-state", "still");
-//     }
+$("#btnSearch").on('click', function (e) {
+    e.preventDefault();
+    var txtEnter = $("#txtEnter").val().trim();
+    topics.push(txtEnter);
+    $('#btnDiv').empty();
+    displayButton();
+})
